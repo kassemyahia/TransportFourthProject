861529174eac788ac6d1e198f988402c2f1f3c87
Kassem Yahia <kassemhajyahia@gmail.com>
2026-09-06 01:21:39 +0300

    Configure automatic MonsterASP deployment

--- TransportFourthProject.Api/Program.cs
+++ TransportFourthProject.Api/Program.cs
@@ -115,6 +115,13 @@ namespace TransportFourthProject.Api
             builder.Services.AddScoped<IAdminAllOperationOnEmployeeTableRepo, AdminAllOperationOnEmployeeTableRepo>();
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
